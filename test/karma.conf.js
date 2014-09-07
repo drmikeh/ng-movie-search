4d618d3deec66d77ed556d895a59966c1623a83b
Michael Hopper <michael.hopper@Altisource.com>
2014-09-06 20:58:42 -0400

    Fixed some build issues.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,6 +28,8 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/ui-router-extras/release/ct-ui-router-extras.js',
+      'bower_components/angular-busy/dist/angular-busy.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
